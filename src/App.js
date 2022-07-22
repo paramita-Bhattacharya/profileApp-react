cc27fe68e6cb4918872898a01c761e62a1fe8434
paramita-Bhattacharya <paramitabhattacharya09@gmail.com>
2022-07-23 07:12:07 +1000

    committed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.module.css'
+import ProfileList from './component/profile/ProfileList'
+
+function App() {
+  return (
+    <div>
+      <ProfileList />
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/profile-data.js
@@ -0,0 +1,26 @@
+import IMG1 from '../assets/paramita.jpg'
+import IMG2 from '../assets/paramita1.jpg'
+import IMG3 from '../assets/paramita2.jpg'
+export const profiles = [
+  {
+    img: IMG1,
+    name: 'Paramita--1',
+    job: 'Web Developer',
+    company: 'Freelancer',
+    link: 'https://www.paramita.digital/',
+  },
+  {
+    img: IMG2,
+    name: 'Paramita--2',
+    job: 'Sales Manager',
+    company: 'LSM Solutions Ltd',
+    link: 'https://www.linkedin.com/feed/',
+  },
+  {
+    img: IMG3,
+    name: 'Paramita--3',
+    job: 'Singer',
+    company: 'Shree shakti',
+    link: 'https://www.youtube.com/channel/UCdlEldDg4rx1ILxYth3VMFg/videos',
+  },
+]
